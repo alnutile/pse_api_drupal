2ed2c2dfdf60acbed08ce138ff014ea1ebfbbc8b
alnutile <alfrednutile@gmail.com>
2014-03-21 12:52:53 +0000

    Issue #12

--- js/controllers/bannersController.js
+++ js/controllers/bannersController.js
@@ -2,7 +2,7 @@ var bannersController = angular.module('bannersController', []);
 
 bannersController.controller('BannersController', ['$scope', '$http', '$location', '$route', '$routeParams', 'BannerService',
     function($scope, $http, $location, $route, $routeParams, BannerService){
-        $scope.interval = 3000;
+        $scope.interval = 10000;
         $scope.slides = BannerService.query();
 
     }]);
